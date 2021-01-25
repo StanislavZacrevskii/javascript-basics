fbc07b0152a82c14fd56e0119ed9362a66519fa7
Extsza.anthill <extsza@anthillagency.com>
2021-01-25 10:17:17 +0200

    homeTask_02

---
+++ HomeTask_02/main.js
@@ -0,0 +1,22 @@
+// Task 1
+let firstInput = +prompt('Input 1:'),
+    secondInput = +prompt('Input 2:');
+
+console.log(firstInput + secondInput);
+
+// Task 2
+// let fullInput = prompt('What is your name?');
+//
+// console.log("Hello " + fullInput + "!");
+// console.log(`Hello ${fullInput}!`);
+
+// Task 3
+// let numberInput = +prompt('Input number:');
+//
+// console.log(numberInput % 2);
+
+// Task 4
+// let numberInput = +prompt('Input a float number:');
+//
+// console.log(parseInt(numberInput));
+// console.log(numberInput - parseInt(numberInput));
