8176a1f2716dcbbdf3279ace1cf374d7e5504e64
Extsza.anthill <extsza@anthillagency.com>
2021-02-01 09:40:07 +0200

    homeTask_03 and homeTask_04

---
+++ HomeTask_03/main.js
@@ -0,0 +1,48 @@
+// Task 1
+// let numberInput = +prompt('Number:');
+//
+// if ((numberInput % 2) === 0) {
+//     console.log("The number is even");
+// } else {
+//     console.log("The number is odd");
+// }
+
+// Task 2
+// let numberInput = +prompt('How old are you?');
+//
+// if ((numberInput <= 14) || (numberInput >= 90)) {
+//     console.log("You can't watch this video!");
+// } else {
+//     console.log("Let's go!");
+// }
+
+// Task 3
+// let numberInput = 0;
+//
+// while ( numberInput <= 10) {
+//     console.log(numberInput);
+//     numberInput += 2;
+// }
+
+// Task 4
+// for (let i = 0; i <= 9; i++) {
+//     if ( i % 2 ===0) continue;
+//     console.log(i);
+// }
+
+// Task 5
+// let userName = prompt("Who's there?", '');
+// if (userName === 'Admin') {
+//     let pass = prompt('Password?', '');
+//     if (pass === 'TheMaster') {
+//         console.log( 'Welcome!' );
+//     } else if (pass === '' || pass === null) {
+//         console.log( 'Canceled' );
+//     } else {
+//         console.log( 'Wrong password' );
+//     }
+// } else if (userName === '' || userName === null) {
+//     console.log( 'Canceled' );
+// } else {
+//     console.log( "I don't know you" );
+// }
---
+++ HomeTask_04/main.js
@@ -0,0 +1,52 @@
+// Task 1
+// function pow(a, b) {
+//     return (a ** b);
+// }
+// let aInput = +prompt('Enter the number');
+// let bInput = +prompt('Enter another number');
+// alert( pow(aInput, bInput) );
+// ------
+// function pow(x, n) {
+//     let result = 1;
+//     for (let i = 0; i < n; i++) {
+//         result *= x;
+//     }
+//     return result;
+// }
+// alert( pow(2, 3) );
+
+// Task 2
+// let aInput = +prompt('Enter the number');
+// let bInput = +prompt('Enter another number');
+// function min(a, b) {
+//     if (a < b) {
+//         alert(a);
+//         return a;
+//     } else {
+//         alert(b);
+//         return b;
+//     }
+// }
+// min(aInput, bInput);
+
+// Task 3
+// let aInput = +prompt('Enter the number');
+// let bInput = +prompt('Enter another number');
+// function max(a, b) {
+//     if (a > b) {
+//         alert(a);
+//         return a;
+//     } else {
+//         alert(b);
+//         return b;
+//     }
+// }
+// max(aInput, bInput);
+
+// Task 4
+
+
+// Task 5
+// (function (message) {
+//     console.log(message);
+// })();
