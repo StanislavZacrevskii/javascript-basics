17ed4456a8be90a9f499c8ba1b1b7bc239b89e74
Extsza.anthill <extsza@anthillagency.com>
2021-02-04 09:59:26 +0200

    homeTask_05

---
+++ HomeTask_05/main.js
@@ -0,0 +1,36 @@
+// Task 1
+//The array "fruits" will be 4 because we did not copy it, but created a reference to it.
+
+// Task 2
+/*let musicArr = ["Jazz", "Blues"];
+console.log(musicArr);
+musicArr.push("Rock-n-Roll");
+console.log(musicArr);
+musicArr.splice(1, 1, "Classics");
+console.log(musicArr);
+musicArr.splice(0, 0, "Rap", "Reggae"); //musicArr.pop("Rap", "Reggae");
+console.log(musicArr);*/
+
+// Task 3
+// function getAverageAge(array) {
+//     return array.reduce((accumulator, item) => accumulator + item, 0) / array.length;
+// }
+//
+// let array = [15, 60, 45, 30];
+// let result = getAverageAge(array);
+// console.log(result);
+
+// Task 4
+/*function filterRange(array, min, max) {
+    return array.filter(
+        (i) => {
+            return i >= min && i < max;
+        }
+    )
+}
+
+let array = [5, 3, 8, 1];
+let filtered = filterRange(array, 1, 4);
+
+console.log(filtered);
+console.log(array);*/
